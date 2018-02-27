91038b1b6393f02ac3d81e4f5779f38afff9d7d9
Iliana Hadzhiatanasova <ih1u16@soton.ac.uk>
2018-02-27 11:38:21 +0000

    Connect to database

--- src/main/java/group33/seg/Main.java
+++ src/main/java/group33/seg/Main.java
@@ -1,7 +1,39 @@
 package group33.seg;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Is maven working? :D ");
+        connectDatabase();
+    }
+
+    public static Connection connectDatabase() {
+        Connection c = null;
+
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        String host = prop.getProperty("DB_HOST");
+        String user = prop.getProperty("DB_USER");
+        String password = prop.getProperty("DB_PASSWORD");
+
+        try {
+            c = DriverManager.getConnection(host, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        System.out.println("Database loaded successfully");
+        return c;
     }
 }
