690ca3eac2ad62a1617d51a486912d9528e8aa79
Iliana Hadzhiatanasova <ih1u16@soton.ac.uk>
2018-02-28 01:51:17 +0000

    Clean up main method: split database connection and config

--- src/main/java/group33/seg/Main.java
+++ src/main/java/group33/seg/Main.java
@@ -1,39 +1,17 @@
 package group33.seg;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import group33.seg.database.DatabaseConfig;
+import group33.seg.database.DatabaseConnection;
+import group33.seg.database.ServerLog;
+
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
-        connectDatabase();
-    }
-
-    public static Connection connectDatabase() {
-        Connection c = null;
-
-        Properties prop = new Properties();
-        try {
-            prop.load(new FileInputStream("config.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-        String host = prop.getProperty("DB_HOST");
-        String user = prop.getProperty("DB_USER");
-        String password = prop.getProperty("DB_PASSWORD");
-
-        try {
-            c = DriverManager.getConnection(host, user, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-
-        System.out.println("Database loaded successfully");
-        return c;
+    public static void main(String[] args) throws SQLException {
+        DatabaseConfig config = new DatabaseConfig("config.properties");
+        DatabaseConnection conn = new DatabaseConnection(config.getHost(), config.getUser(), config.getPassword());
+        conn.connectDatabase();
     }
 }
+
+
