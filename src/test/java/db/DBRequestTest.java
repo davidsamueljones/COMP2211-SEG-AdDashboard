6ba3ed2c9a3c9793174148929af2106c502f7877
Unknown <harryjgbrown@gmail.com>
2018-03-03 21:09:51 +0000

    adding database interface tests

---
+++ src/test/java/db/DBRequestTest.java
@@ -0,0 +1,34 @@
+package db;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import group33.seg.db.DBRequest;
+
+public class DBRequestTest {
+
+    @Test
+    public void dbRequestEqualityTest () throws Exception {
+        DBRequest r1 = new DBRequest(DBRequest.impression);
+
+        r1.setAxes(DBRequest.impression, "date", "count(*)", "5 day");
+        r1.addEqualConstraint(DBRequest.impression, "gender", "Male");
+
+        DBRequest r2 = new DBRequest(DBRequest.impression);
+
+        r2.setAxes(DBRequest.impression, "date", "count(*)", "5 day");
+
+        assertTrue(!r1.equals(r2));
+        assertTrue(r1.hashCode() != r2.hashCode());
+        assertTrue(!r1.getSql().equals(r2.hashCode()));
+
+
+        r2.addEqualConstraint(DBRequest.impression, "gender", "Male");
+
+
+        assertTrue(r1.equals(r2));
+        assertTrue(r1.hashCode() == r2.hashCode());
+        assertTrue(r1.getSql().equals(r2.getSql()));
+        
+    }
+}
