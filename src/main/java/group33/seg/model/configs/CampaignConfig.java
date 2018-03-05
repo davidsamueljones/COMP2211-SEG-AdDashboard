1110cdafcdd438dbaf9c90c1fc5135626684f496
David Jones <davidsamueljones@icloud.com>
2018-03-05 22:11:38 +0000

    Changed to not require a database target
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/model/configs/CampaignConfig.java
+++ src/main/java/group33/seg/model/configs/CampaignConfig.java
@@ -2,18 +2,13 @@ package group33.seg.model.configs;
 
 public class CampaignConfig {
   private String name;
-  private String database;
 
-  public CampaignConfig(String name, String database) {
+  public CampaignConfig(String name) {
     this.name = name;
-    this.database = database;
   }
 
   public String getName() {
     return name;
   }
-
-  public String getDatabase() {
-    return database;
-  }
+  
 }
