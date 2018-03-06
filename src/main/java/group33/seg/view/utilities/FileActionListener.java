7e6c9554901e751707050039932527e70eceab1a
David Jones <davidsamueljones@icloud.com>
2018-03-06 02:02:22 +0000

    Added configurable file browser action
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

---
+++ src/main/java/group33/seg/view/utilities/FileActionListener.java
@@ -0,0 +1,43 @@
+package group33.seg.view.utilities;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+
+public class FileActionListener implements ActionListener {
+  private Map<Object, JTextField> mappings = new HashMap<>();
+  private JFileChooser fileChooser = new JFileChooser();
+  private Component parent;
+  
+  public FileActionListener(Component parent, int selectionMode) {
+    this.parent = parent;
+    fileChooser.setFileSelectionMode(selectionMode);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    JTextField txt = mappings.get(e.getSource());
+    if (txt == null) {
+      System.err.print("Object does not have a corresponding field to update");
+      return;
+    }
+
+    int res = fileChooser.showOpenDialog(parent);
+    if (res == JFileChooser.APPROVE_OPTION) {
+      txt.setText(fileChooser.getSelectedFile().getAbsolutePath());
+    }
+  }
+
+  public void addMapping(Object object, JTextField txt) {
+    mappings.put(object, txt);
+  }
+
+  public JFileChooser getFileChooser() {
+    return fileChooser;
+  }
+
+}
