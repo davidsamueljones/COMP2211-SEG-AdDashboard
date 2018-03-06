9c10fd0ca6ceedd47243060be1ad86065a9b90e7
David Jones <davidsamueljones@icloud.com>
2018-03-06 08:00:57 +0000

    Added skeleton gui for test panels
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

---
+++ src/main/java/group33/seg/view/increment1/GraphGeneratorPanel.java
@@ -0,0 +1,60 @@
+package group33.seg.view.increment1;
+
+import javax.swing.JPanel;
+import group33.seg.model.types.Interval;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JComboBox;
+import java.awt.Insets;
+import javax.swing.JButton;
+
+public class GraphGeneratorPanel extends JPanel {
+  private JButton btnGenerateImpressionGraph;
+
+  /**
+   * Create the panel.
+   */
+  public GraphGeneratorPanel() {
+
+    initGUI();
+  }
+
+  private void initGUI() {
+    GridBagLayout gridBagLayout = new GridBagLayout();
+    gridBagLayout.columnWidths = new int[] {0, 0, 0};
+    gridBagLayout.rowHeights = new int[] {0, 0, 0};
+    gridBagLayout.columnWeights = new double[] {0.0, 1.0, Double.MIN_VALUE};
+    gridBagLayout.rowWeights = new double[] {0.0, 0.0, Double.MIN_VALUE};
+    setLayout(gridBagLayout);
+
+    JLabel lblInterval = new JLabel("Interval:");
+    GridBagConstraints gbc_lblInterval = new GridBagConstraints();
+    gbc_lblInterval.insets = new Insets(0, 0, 5, 5);
+    gbc_lblInterval.anchor = GridBagConstraints.EAST;
+    gbc_lblInterval.gridx = 0;
+    gbc_lblInterval.gridy = 0;
+    add(lblInterval, gbc_lblInterval);
+
+    JComboBox<Interval> cboInterval = new JComboBox<>();
+    GridBagConstraints gbc_cboInterval = new GridBagConstraints();
+    gbc_cboInterval.insets = new Insets(0, 0, 5, 0);
+    gbc_cboInterval.fill = GridBagConstraints.HORIZONTAL;
+    gbc_cboInterval.gridx = 1;
+    gbc_cboInterval.gridy = 0;
+    add(cboInterval, gbc_cboInterval);
+
+    for (Interval interval : Interval.values()) {
+      cboInterval.addItem(interval);
+    }
+
+    btnGenerateImpressionGraph = new JButton("Generate Impression Graph");
+    GridBagConstraints gbc_btnGenerateImpressionGraph = new GridBagConstraints();
+    gbc_btnGenerateImpressionGraph.fill = GridBagConstraints.HORIZONTAL;
+    gbc_btnGenerateImpressionGraph.gridwidth = 2;
+    gbc_btnGenerateImpressionGraph.gridx = 0;
+    gbc_btnGenerateImpressionGraph.gridy = 1;
+    add(btnGenerateImpressionGraph, gbc_btnGenerateImpressionGraph);
+  }
+
+}
