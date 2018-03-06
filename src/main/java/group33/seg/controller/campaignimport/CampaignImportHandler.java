ecf98db0dfc883aa00028e97e778b5176ca3adec
David Jones <davidsamueljones@icloud.com>
2018-03-06 06:41:19 +0000

    Added proper interrupt handling, error handling  and progress updating
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/controller/campaignimport/CampaignImportHandler.java
+++ src/main/java/group33/seg/controller/campaignimport/CampaignImportHandler.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import group33.seg.controller.database.ClickLog;
 import group33.seg.controller.database.DatabaseConfig;
 import group33.seg.controller.database.DatabaseConnection;
+import group33.seg.controller.database.DatabaseTable;
 import group33.seg.controller.database.ImpressionLog;
 import group33.seg.controller.database.ServerLog;
 import group33.seg.controller.utilities.ErrorBuilder;
@@ -16,28 +17,28 @@ import java.sql.SQLException;
 import java.util.HashSet;
 
 /**
- * Controller class handling import of campaign data using a separate thread and the 
- * current database connection. Use listening infrastructure to communicate any external updates.
+ * Controller class handling import of campaign data using a separate thread and the current
+ * database connection. Use listening infrastructure to communicate any external updates.
  */
 public class CampaignImportHandler {
   /** Thread safety lock for importedCampaign */
   private final Object lockImportedCampaign = new Object();
- 
+
   /** Last campaign to be imported */
   private CampaignConfig importedCampaign = null;
 
   /** Current progress of import */
   private volatile int progress;
- 
+
   /** Reference thread used for importing */
   private Thread threadImport;
-  
+
   /** Listeners to alert about import progress */
   private final Set<ProgressListener> progressListeners = new HashSet<>();
 
-  /** Error builder to store errors pertaining to the last import */ 
+  /** Error builder to store errors pertaining to the last import */
   private ErrorBuilder eb = new ErrorBuilder();
-  
+
   /**
    * Import CSV files into tables using the current database connection. The imported data will
    * replace any data residing in the database. Progress listeners are kept updated on status of
@@ -47,79 +48,54 @@ public class CampaignImportHandler {
    * @param importConfig Campaign information required for import.
    * @return Whether import started successfully
    */
-  public boolean doImport(CampaignImportConfig importConfig) {    
+  public boolean doImport(CampaignImportConfig importConfig) {
     // Refresh error builder for this import, whilst validating config
     eb = importConfig.validate();
-    
+
     // Error in configuration
     if (eb.isError()) {
-      return false; 
-    }  
+      return false;
+    }
     // Cannot start another import whilst another is ongoing
     if (isOngoing()) {
       eb.addError("An import is already ongoing");
       return false;
     }
-    
+
     // Handle import on a separate thread
     threadImport = new Thread(new Runnable() {
       @Override
       public void run() {
+        // Alert listeners that import is starting
+        alertStart();
+        // Reset import progress
+        updateProgress(0);
+
+        // TODO: TEMPORARY - Use interface database connection
+        DatabaseConfig config = new DatabaseConfig("config.properties");
+        DatabaseConnection dbConn =
+            new DatabaseConnection(config.getHost(), config.getUser(), config.getPassword());
+        dbConn.connectDatabase();
+        Connection conn = dbConn.connectDatabase();
+
+        // TODO: Drop old table
         try {
-          // Alert listeners that import is starting
-          alertStart();
+          // Import all tables
+          importTable(new ClickLog(), conn, importConfig.pathClickLog, 0.33);
+          importTable(new ImpressionLog(), conn, importConfig.pathImpressionLog, 0.33);
+          importTable(new ServerLog(), conn, importConfig.pathServerLog, 0.33);
 
-          // TODO: TEMPORARY - Use interface database connection
-          DatabaseConfig config = new DatabaseConfig("config.properties");
-          DatabaseConnection dbConn = new DatabaseConnection(config.getHost(), config.getUser(), config.getPassword());
-          dbConn.connectDatabase();
-          Connection conn = dbConn.connectDatabase();
-          
-          // TODO: Improve update progress, most likely by changing import functions
-          updateProgress(0);
-          // TODO: Drop old table
-          updateProgress(1);
-          
-          try {
-            // Import click log
-            ClickLog clickLog = new ClickLog();
-            clickLog.createTable(conn);
-            clickLog.importCSV(conn, importConfig.pathClickLog);
-            updateProgress(33);
- 
-            if (Thread.interrupted()) {
-              throw new InterruptedException();
-            }
-            
-            // Import impression log
-            ImpressionLog imprlog = new ImpressionLog();
-            imprlog.createTable(conn);
-            imprlog.importCSV(conn, importConfig.pathImpressionLog);
-            updateProgress(66);
-            
-            if (Thread.interrupted()) {
-              throw new InterruptedException();
-            }
-            
-            // Import server log
-            ServerLog serverLog = new ServerLog();
-            serverLog.createTable(conn);
-            serverLog.importCSV(conn, importConfig.pathServerLog);
-            updateProgress(100);
-            
-            // Create campaign configuration (storing as last import)
-            setImportedCampaign(new CampaignConfig(importConfig.campaignName));
-            // Alert listeners that import is finished
-            alertFinished(true);
-          } catch (SQLException e) {
-            alertFinished(false);
-            e.printStackTrace();
-          }
-
+          // Create campaign configuration (storing as last import)
+          setImportedCampaign(new CampaignConfig(importConfig.campaignName));
+          // Alert listeners that import is finished
+          alertFinished(true);
         } catch (InterruptedException e) {
-          // drop any half imported data
+          // TODO: Drop half imported data
           alertCancelled();
+        } catch (ImportException e) {
+          alertFinished(false);
         }
+        
         threadImport = null;
       }
     });
@@ -130,6 +106,60 @@ public class CampaignImportHandler {
     return true;
   }
 
+  /**
+   * Use a worker thread to import a given database type with the given file.
+   * 
+   * @param table Table type to import to
+   * @param conn Database connection
+   * @param path Path to file to import
+   * @param weight Weighting for progress updates
+   */
+  private void importTable(DatabaseTable table, Connection conn, String path, double weight)
+      throws InterruptedException {
+    DatabaseTableImporter importer = new DatabaseTableImporter();
+    final int curProgress = progress;
+  
+    // Ensure table is created 
+    try {
+      table.createTable(conn);
+    } catch (SQLException e) {
+      eb.addError("Database error, consult your administrator");
+      throw new ImportException();
+    }
+    
+    // Create worker thread handling import
+    Thread worker = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          importer.importCSV(table, conn, path);
+        } catch (Exception e) {
+          // do nothing, let main import thread handle
+        }
+      }
+    });
+
+    // Start worker
+    worker.start();
+    // Wait for worker to finish
+    while (worker.isAlive()) {
+      Thread.sleep(100);
+      // Check for interrupts
+      if (Thread.interrupted()) {
+        worker.interrupt();
+        worker.join(0);
+        throw new InterruptedException();
+      }
+      // Update progress
+      updateProgress(curProgress + (int) (importer.getProgress() * weight));
+    }
+    // Check if import was successful
+    if (!importer.isSuccessful()) {
+      eb.addError(String.format(
+          "Error importing file '%s', check that the file is of the correct format", path));
+      throw new ImportException();
+    }
+  }
 
   /**
    * Cancel an ongoing import (if one is ongoing). Let importer control how cancellation occurs.
@@ -152,14 +182,14 @@ public class CampaignImportHandler {
     return false;
   }
 
-  
+
   /**
    * @return Whether an import is ongoing
    */
   public boolean isOngoing() {
     return (threadImport != null && threadImport.isAlive());
   }
-  
+
   /**
    * @return Current progress level
    */
@@ -202,7 +232,7 @@ public class CampaignImportHandler {
   public void removeProgressListener(ProgressListener progressListener) {
     progressListeners.remove(progressListener);
   }
-  
+
   /**
    * Helper function to alert all listeners that an import has started.
    */
@@ -243,7 +273,7 @@ public class CampaignImportHandler {
       listener.progressUpdate(progress);
     }
   }
-  
+
   /**
    * @return Error builder pertaining to last import
    */
@@ -275,8 +305,8 @@ public class CampaignImportHandler {
       this.pathImpressionLog = pathImpressionLog;
       this.pathServerLog = pathServerLog;
     }
-    
-    
+
+
     public ErrorBuilder validate() {
       ErrorBuilder eb = new ErrorBuilder();
       if (campaignName.isEmpty()) {
@@ -291,8 +321,15 @@ public class CampaignImportHandler {
       if (!Files.exists(Paths.get(pathServerLog))) {
         eb.addError("Server log path does not exist");
       }
-      return eb; 
+      return eb;
     }
+  }
+  
+  /**
+   * Unchecked exception for importing failures
+   */
+  public class ImportException extends RuntimeException {
+    private static final long serialVersionUID = -3767480036135704125L;
   }
 
 }
