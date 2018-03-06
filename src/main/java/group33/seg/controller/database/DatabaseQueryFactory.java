a35b39477afb323b33009fe4d0d3c4a5691a30fb
David Jones <davidsamueljones@icloud.com>
2018-03-06 08:44:25 +0000

    Made DatabaseQueryFactory static, corrected distinction between statistic and metric
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/controller/database/DatabaseQueryFactory.java
+++ src/main/java/group33/seg/controller/database/DatabaseQueryFactory.java
@@ -3,41 +3,52 @@ package group33.seg.controller.database;
 import group33.seg.model.configs.MetricQuery;
 import group33.seg.model.types.Metric;
 import java.util.HashMap;
+import java.util.Map;
 
 public class DatabaseQueryFactory {
-  private HashMap<Metric, String> metricQueries = new HashMap<>();
-  private HashMap<Metric, String> statisticQueries = new HashMap<>();
+  private static final Map<Metric, String> graphQueries = new HashMap<>();
+  private static final Map<Metric, String> statisticQueries = new HashMap<>();
 
-  public DatabaseQueryFactory() {
-    metricQueries = new HashMap<>();
-    statisticQueries = new HashMap<>();
+  static {
+    createGraphQueries();
+    createStatisticTemplates();
+  }
 
-    // add sql statistic templates
-    statisticQueries.put(
-        Metric.IMPRESSIONS,
+  /**
+   * Define and store templates for every graph metric type.
+   */
+  private static void createGraphQueries() {
+    graphQueries.put(Metric.IMPRESSIONS,
         "SELECT 'all' as xaxis, count(*) as yaxis from impression_log group by xaxis;");
+  }
 
-    // add sql metric templates
-    metricQueries.put(
-        Metric.IMPRESSIONS,
+  /**
+   * Define and store templates for every statistic metric type.
+   */
+  private static void createStatisticTemplates() {
+    statisticQueries.put(Metric.IMPRESSIONS,
         "SELECT date_trunc('<interval>', date) as xaxis, count(*) as yaxis from impression_log group by xaxis;");
   }
 
   /**
-   * @param request MetricQuery request object
-   * @return SQL String representing given MetricQuery
+   * Using a metric query request object, generate its corresponding SQL.
+   * 
+   * @param request Query to generate SQL for
+   * @return SQL generated from given MetricQuery
    */
-  public String generateSql(MetricQuery request) {
+  public static String generateSQL(MetricQuery request) {
     String sql;
 
-    if (request.getInterval() != null) {
-      sql = metricQueries.get(request.getMetric());
-      sql = sql.replace("<interval>", request.getInterval().getValue());
+    if (request.interval != null) {
+      // Create graph query
+      sql = graphQueries.get(request.metric);
+      sql = sql.replace("<interval>", request.interval.getValue());
     } else {
-      sql = statisticQueries.get(request.getMetric());
+      // Create statistic query
+      sql = statisticQueries.get(request.metric);
     }
 
-    // todo add filters
     return sql;
   }
+
 }
