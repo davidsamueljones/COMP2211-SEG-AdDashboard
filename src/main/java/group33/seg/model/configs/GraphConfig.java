a15ec47797f0c07733cf9c031a1f8a4fde498b60
David Jones <davidsamueljones@icloud.com>
2018-03-21 15:10:52 +0000

    Added graph configurations
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/model/configs/GraphConfig.java
+++ src/main/java/group33/seg/model/configs/GraphConfig.java
@@ -1,7 +1,21 @@
 package group33.seg.model.configs;
 
+/**
+ * Structure-like class for constructing a generic graph. All variables are public to allow
+ * for easy structure access.
+ */
 public abstract class GraphConfig {
-  // identifier
-  // title
-  // axis-titles?
+  
+  /** Storage identifier of graph */
+  public String identifier;
+  
+  /** Main chart title */
+  public String title;
+  
+  /** Title of X Axis */
+  public String xAxisTitle;
+  
+  /** Title of Y Axis */
+  public String yAxisTitle;
+
 }
