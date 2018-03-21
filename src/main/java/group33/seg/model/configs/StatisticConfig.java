ea8401e2306267aa224930a3988137c29d348849
David Jones <davidsamueljones@icloud.com>
2018-03-21 15:18:36 +0000

    Added statistic config
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/model/configs/StatisticConfig.java
+++ src/main/java/group33/seg/model/configs/StatisticConfig.java
@@ -1,8 +1,18 @@
 package group33.seg.model.configs;
 
+/**
+ * Structure-like class for constructing a statistic configuration. All variables are public to allow
+ * for easy structure access.
+ */
 public class StatisticConfig {
-  // identifier
-  // hide
+  
+  /** Storage identifier of statistic */
+  public String identifier;
+  
+  /** Whether to hide statistic from display */
+  public boolean hide;
+  
+  /** Query to fetch data with, should use no grouping and no specific metric */
+  public MetricQuery query;
 
-  // metric query (unset metric)
 }
