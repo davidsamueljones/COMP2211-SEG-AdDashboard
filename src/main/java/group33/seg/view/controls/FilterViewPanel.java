24ac139c3948a5bc30091b8f295eb05590aa3dda
David Jones <davidsamueljones@icloud.com>
2018-03-23 00:30:04 +0000

    Replaced list with text area for filter display
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/view/controls/FilterViewPanel.java
+++ src/main/java/group33/seg/view/controls/FilterViewPanel.java
@@ -1,10 +1,16 @@
 package group33.seg.view.controls;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
+import group33.seg.model.configs.CampaignConfig;
+import group33.seg.model.configs.FilterConfig;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import javax.swing.JComboBox;
 import java.awt.Insets;
@@ -51,27 +57,28 @@ public class FilterViewPanel extends JPanel {
     gbc_lblCampaign.anchor = GridBagConstraints.EAST;
     gbc_lblCampaign.gridx = 0;
     gbc_lblCampaign.gridy = 0;
-    add(lblCampaign, gbc_lblCampaign);
+    //add(lblCampaign, gbc_lblCampaign);
 
-    // TODO: Chose type for JComboBox (String or Campaign)
-    JComboBox cboCampaign = new JComboBox();
+    JComboBox<CampaignConfig> cboCampaign = new JComboBox<>();
     GridBagConstraints gbc_cboCampaign = new GridBagConstraints();
     gbc_cboCampaign.insets = new Insets(0, 0, 5, 0);
     gbc_cboCampaign.fill = GridBagConstraints.HORIZONTAL;
     gbc_cboCampaign.gridx = 1;
     gbc_cboCampaign.gridy = 0;
-    add(cboCampaign, gbc_cboCampaign);
+    //add(cboCampaign, gbc_cboCampaign);
 
-    // TODO: Chose type for JList (String or Filter)
-    JList lstFilter = new JList();
-    lstFilter.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
-    GridBagConstraints gbc_lstFilter = new GridBagConstraints();
-    gbc_lstFilter.gridwidth = 2;
-    gbc_lstFilter.insets = new Insets(0, 0, 5, 0);
-    gbc_lstFilter.fill = GridBagConstraints.BOTH;
-    gbc_lstFilter.gridx = 0;
-    gbc_lstFilter.gridy = 1;
-    add(lstFilter, gbc_lstFilter);
+    JTextArea txtFilter = new JTextArea();
+    txtFilter.setEditable(false);
+    JScrollPane scrFilter = new JScrollPane(txtFilter);
+    scrFilter.setPreferredSize(new Dimension(0, scrFilter.getPreferredSize().height));
+    scrFilter.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+    GridBagConstraints gbc_txtFilter = new GridBagConstraints();
+    gbc_txtFilter.gridwidth = 2;
+    gbc_txtFilter.insets = new Insets(0, 0, 5, 0);
+    gbc_txtFilter.fill = GridBagConstraints.BOTH;
+    gbc_txtFilter.gridx = 0;
+    gbc_txtFilter.gridy = 1;
+    add(scrFilter, gbc_txtFilter);
 
     btnModifyFilter = new JButton("Modify Filter");
     GridBagConstraints gbc_btnModifyFilter = new GridBagConstraints();
