f3b758c7b747890bfd859f09b10f94456696f084
David Jones <davidsamueljones@icloud.com>
2018-03-27 21:03:31 +0100

    More work on graph handler for handling a new/updated graph configuration
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/controller/handlers/GraphHandler.java
+++ src/main/java/group33/seg/controller/handlers/GraphHandler.java
@@ -1,6 +1,7 @@
 package group33.seg.controller.handlers;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import group33.seg.controller.DashboardController.DashboardMVC;
@@ -18,7 +19,10 @@ public class GraphHandler {
   private final DashboardMVC mvc;
 
   /** Graph being controlled */
-  private Graph graph;
+  private Graph graphView;
+
+  /** Currently loaded graph */
+  private GraphConfig graph;
 
   /**
    * Instantiate a graph handler.
@@ -29,20 +33,138 @@ public class GraphHandler {
     this.mvc = mvc;
   }
 
-  public void setGraph(Graph graph) {
-    this.graph = graph;
-    graph.setFontScale(scale); // FIXME: Instead fetch theme here
+  /**
+   * @param graphView View to update when graph updates occur
+   */
+  public void setGraphView(Graph graphView) {
+    this.graphView = graphView;
+    // FIXME: Instead fetch theme here
+    graphView.setFontScale(scale);
   }
 
-  private void loadLineGraph(LineGraphConfig graph) {
+  /**
+   * Update the line graph displayed in the view. As an update, behaviour means that if the
+   * currently displayed graph is the same as the input graph, only changes will be reflected. In
+   * the case that the current graph does not exist or is different, behaviour is the same as that
+   * of loadLineGraph.
+   * 
+   * @param graph Graph to update the view with
+   */
+  public void updateLineGraph(LineGraphConfig graph) {
+    if (this.graph == null || !this.graph.uuid.equals(graph.uuid)) {
+      // Load whole graph freshly
+      loadLineGraph(graph);
+    } else {
+      // Update current graph
+      setGraphProperties(graph);
+      updateGraphLines(graph.lines);
+      this.graph = graph;
+    }
+  }
+
+  public void loadLineGraph(LineGraphConfig graph) {
+    clearGraph();
+    setGraphProperties(graph);
+    loadGraphLines(graph.lines);
     // LineGraphConfig graph = wizard.getGraph();
     // if (graph != null) {
     // refreshGraphs(graph.identifier);
     // }
+    // TODO
   }
 
-  private void currentGraph() {
+  /**
+   * Update the graph's view's properties, this includes information such as title and axis labels.
+   * No data fetches should occur here.
+   * 
+   * @param graph Graph properties to load
+   */
+  public void setGraphProperties(GraphConfig graph) {
+    // TODO: CALL VIEW UPDATES FOR TITLE/AXIS LABELS
+  }
 
+  /**
+   * 
+   * @param lines
+   */
+  public void updateGraphLines(List<LineConfig> lines) {
+    // Get existing lines in graph
+    List<LineConfig> exLines = null;
+    if (this.graph instanceof LineGraphConfig) {
+      exLines = ((LineGraphConfig) this.graph).lines;
+    }
+    if (exLines == null) {
+      // No existing found, must load all
+      loadGraphLines(lines);
+    } else {
+      // Remove existing lines that no longer exist or are hidden after update
+      for (LineConfig exLine : exLines) {
+        boolean remove = false;
+        int idx = lines.indexOf(exLine);
+        if (idx >= 0) {
+          LineConfig line = lines.get(idx);
+          if ((exLine.hide != line.hide) && line.hide) {
+            remove = true;
+          }
+        } else {
+          remove = true;
+        }
+        if (remove) {
+          removeLine(exLine);
+        }
+      }
+      // Update and add new lines
+      for (LineConfig line : lines) {
+        int idx = exLines.indexOf(line);
+        if (idx >= 0) {
+          addLine(line, getLineUpdate(exLines.get(idx), line));
+        } else {
+          addLine(line);
+        }
+      }
+    }
+  }
+
+
+
+  public void loadGraphLines(List<LineConfig> lines) {
+    for (LineConfig line : lines) {
+      addLine(line);
+    }
+
+    // LineGraphConfig graph = wizard.getGraph();
+    // if (graph != null) {
+    // refreshGraphs(graph.identifier);
+    // }
+    // TODO
+  }
+
+
+
+  public void clearLines() {
+    clearLines(null);
+  }
+
+  private void clearLines(List<LineConfig> lines) {
+
+  }
+
+
+  public void addLine(LineConfig line) {
+    addLine(line, Update.FULL);
+  }
+
+  public void addLine(LineConfig line, Update update) {
+    if (update == Update.FULL || update == Update.DATA) {
+      // TODO: Query data and do line data update
+    }
+    if (update == Update.FULL || update == Update.PROPERTIES) {
+      // TODO: Update properties
+    }
+  }
+
+  public void removeLine(LineConfig line) {
+    // TODO: Use the unique identifier to remove line from graph view
   }
 
 
@@ -52,11 +174,11 @@ public class GraphHandler {
     MetricQuery query = new MetricQuery(Metric.IMPRESSIONS, interval, null);
     List<Pair<String, Integer>> data = db.getQueryResponse(query).getResult();
 
-    graph.addLine(data);
+    graphView.addLine(data);
   }
 
   public void clearGraph() {
-    graph.clearLines();
+    graphView.clearLines();
   }
 
   // FIXME: Can remove when setting by theme
@@ -66,6 +188,15 @@ public class GraphHandler {
     this.scale = scale;
   }
 
+  /**
+   * Given an original and an updated line configuration, determine what type of update is required
+   * to update the graph in the least cost-manner. As in, if only line properties have changed it is
+   * best to not requery data due to computational cost.
+   * 
+   * @param original Line configuration to use as base
+   * @param updated Line configuration to compare with base
+   * @return Update type as represented by Update enumeration
+   */
   public static Update getLineUpdate(LineConfig original, LineConfig updated) {
     // Check for any changes in querying that may change data
     boolean data = true;
@@ -95,8 +226,14 @@ public class GraphHandler {
     return Update.NOTHING;
   }
 
+  /**
+   * Enumeration of what type of update has occurred to a graph or graph element.
+   */
   public enum Update {
-    FULL, PROPERTIES, DATA, NOTHING;
+    FULL, /* Both properties and data have changed */
+    PROPERTIES, /* Just properties have changed */
+    DATA, /* Just data has changed */
+    NOTHING; /* No changes */
   }
 
 }
