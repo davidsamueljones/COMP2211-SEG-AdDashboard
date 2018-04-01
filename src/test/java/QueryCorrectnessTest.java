2d83b089b60251fc824019597ceeb31d01272ed5
Unknown <harryjgbrown@gmail.com>
2018-04-01 13:50:39 +0100

    fixed broken test and added cpa and cpm

--- src/test/java/QueryCorrectnessTest.java
+++ src/test/java/QueryCorrectnessTest.java
@@ -68,7 +68,8 @@ public class QueryCorrectnessTest {
 
   @Test
   public void impressionsTest() {
-    List<Pair<String, Integer>> expectedResponse = response("all", 50);
+    List<Pair<String, Integer>> expectedResponse = new LinkedList<>();
+    expectedResponse.add(new Pair<>("all", 50));
 
     MetricQuery statQuery = new MetricQuery(Metric.IMPRESSIONS, null, null);
     List<Pair<String, Integer>> response = databaseHandler.getQueryResponse(statQuery).getResult();
@@ -141,6 +142,93 @@ public class QueryCorrectnessTest {
 
     MetricQuery graphQuery = new MetricQuery(Metric.CONVERSIONS, Interval.WEEK, null);
     List<Pair<String, Integer>> weekResponse = databaseHandler.getQueryResponse(graphQuery).getResult();
+
+    assertTrue("Time interval test wrong for clicks metric", weekResponse.equals(expectedWeekResponse));
+  }
+
+  @Test
+  public void cpaTest () {
+    List<Pair<String,Integer>> expectedResponse = new LinkedList<>();
+    expectedResponse.add(new Pair<>("all", 109));
+
+    MetricQuery statQuery = new MetricQuery(Metric.CPA, null, null);
+    List<Pair<String, Integer>> response = databaseHandler.getQueryResponse(statQuery).getResult();
+    
+    assertTrue("Statistic wrong for CPA, should have been 109 but was " + response.get(0).value, response.equals(expectedResponse));
+
+    List<Pair<String,Integer>> expectedWeekResponse = new LinkedList<>();
+    expectedWeekResponse.add(new Pair<>("2014-12-29 00:00:00", 0));
+    expectedWeekResponse.add(new Pair<>("2015-01-05 00:00:00", 0));
+    expectedWeekResponse.add(new Pair<>("2015-01-12 00:00:00", 0));
+    expectedWeekResponse.add(new Pair<>("2015-01-19 00:00:00", 0));
+    expectedWeekResponse.add(new Pair<>("2015-01-26 00:00:00", 0));
+    expectedWeekResponse.add(new Pair<>("2015-02-02 00:00:00", 58));
+    expectedWeekResponse.add(new Pair<>("2015-02-09 00:00:00", 0));
+    expectedWeekResponse.add(new Pair<>("2015-02-16 00:00:00", 0));
+    expectedWeekResponse.add(new Pair<>("2015-02-23 00:00:00", 46));
+
+    MetricQuery graphQuery = new MetricQuery(Metric.CPA, Interval.WEEK, null);
+    List<Pair<String, Integer>> weekResponse = databaseHandler.getQueryResponse(graphQuery).getResult();
+
+    assertTrue("Time interval test wrong for CPA metric", weekResponse.equals(expectedWeekResponse));
+  }
+
+  @Test
+  public void cpcTest () {
+    List<Pair<String,Integer>> expectedResponse = new LinkedList<>();
+    expectedResponse.add(new Pair<>("all", 6));
+
+    MetricQuery statQuery = new MetricQuery(Metric.CPC, null, null);
+    List<Pair<String, Integer>> response = databaseHandler.getQueryResponse(statQuery).getResult();
+    
+    assertTrue("Statistic wrong for CPC, should have been 6 but was " + response.get(0).value, response.equals(expectedResponse));
+
+    List<Pair<String,Integer>> expectedWeekResponse = new LinkedList<>();
+    expectedWeekResponse.add(new Pair<>("2015-01-05 00:00:00", 10));
+    expectedWeekResponse.add(new Pair<>("2015-01-12 00:00:00", 10));
+    expectedWeekResponse.add(new Pair<>("2015-01-19 00:00:00", 4));
+    expectedWeekResponse.add(new Pair<>("2015-01-26 00:00:00", 4));
+    expectedWeekResponse.add(new Pair<>("2015-02-02 00:00:00", 5));
+    expectedWeekResponse.add(new Pair<>("2015-02-09 00:00:00", 7));
+    expectedWeekResponse.add(new Pair<>("2015-02-16 00:00:00", 7));
+    expectedWeekResponse.add(new Pair<>("2015-02-23 00:00:00", 5));
+
+    MetricQuery graphQuery = new MetricQuery(Metric.CPC, Interval.WEEK, null);
+    List<Pair<String, Integer>> weekResponse = databaseHandler.getQueryResponse(graphQuery).getResult();
+
+    for(Pair p : weekResponse){
+      System.out.println(p.key + " " + p.value);
+    }
+
+    assertTrue("Time interval test wrong for clicks metric", weekResponse.equals(expectedWeekResponse));
+  }
+
+  @Test
+  public void cpmTest () {
+    List<Pair<String,Integer>> expectedResponse = new LinkedList<>();
+    expectedResponse.add(new Pair<>("all", 1));
+
+    MetricQuery statQuery = new MetricQuery(Metric.CPM, null, null);
+    List<Pair<String, Integer>> response = databaseHandler.getQueryResponse(statQuery).getResult();
+    
+    assertTrue("Statistic wrong for CPM, should have been 1 but was " + response.get(0).value, response.equals(expectedResponse));
+
+    List<Pair<String,Integer>> expectedWeekResponse = new LinkedList<>();
+    expectedWeekResponse.add(new Pair<>("2015-01-05 00:00:00", 10));
+    expectedWeekResponse.add(new Pair<>("2015-01-12 00:00:00", 10));
+    expectedWeekResponse.add(new Pair<>("2015-01-19 00:00:00", 4));
+    expectedWeekResponse.add(new Pair<>("2015-01-26 00:00:00", 4));
+    expectedWeekResponse.add(new Pair<>("2015-02-02 00:00:00", 5));
+    expectedWeekResponse.add(new Pair<>("2015-02-09 00:00:00", 7));
+    expectedWeekResponse.add(new Pair<>("2015-02-16 00:00:00", 7));
+    expectedWeekResponse.add(new Pair<>("2015-02-23 00:00:00", 5));
+
+    MetricQuery graphQuery = new MetricQuery(Metric.CPM, Interval.WEEK, null);
+    List<Pair<String, Integer>> weekResponse = databaseHandler.getQueryResponse(graphQuery).getResult();
+
+    for(Pair p : weekResponse){
+      System.out.println(p.key + " " + p.value);
+    }
 
     assertTrue("Time interval test wrong for clicks metric", weekResponse.equals(expectedWeekResponse));
   }
