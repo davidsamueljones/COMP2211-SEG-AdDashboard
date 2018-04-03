508d5ed58c478206fd221d6041d181507029e860
David Jones <davidsamueljones@icloud.com>
2018-04-03 01:42:07 +0100

    Modified testing to be in line with handler changes
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/test/java/QueryCorrectnessTest.java
+++ src/test/java/QueryCorrectnessTest.java
@@ -6,11 +6,14 @@ import java.util.LinkedList;
 import java.util.List;
 import group33.seg.model.configs.*;
 import org.junit.Test;
+import group33.seg.controller.DashboardController;
+import group33.seg.controller.database.DatabaseConfig;
 import group33.seg.controller.handlers.CampaignImportHandler;
 import group33.seg.controller.handlers.DatabaseHandler;
 import group33.seg.model.types.Interval;
 import group33.seg.model.types.Metric;
 import group33.seg.model.types.Pair;
+import group33.seg.model.workspace.Workspace;
 
 /** Tests for correctness of queries on a small set of example data */
 public class QueryCorrectnessTest {
@@ -23,27 +26,30 @@ public class QueryCorrectnessTest {
 
   private static final String DATABASE_CREDENTIALS = "src/test/resources/test.properties";
 
+  private DashboardController controller;
   private DatabaseHandler databaseHandler;
   private CampaignConfig campaign;
 
   /** Initialise tables and data for test */
   public QueryCorrectnessTest() throws FileNotFoundException {
-    try {
-
-      // init tables
+    try {    
+      // Initialise a controller for these tests (no model or view needed)
+      controller = new DashboardController(null, null);
+      controller.database.refreshConnections(new DatabaseConfig(DATABASE_CREDENTIALS), 5);
+      
+      // Do import using custom test configuration
       CampaignImportConfig importConfig = new CampaignImportConfig(CAMPAIGN_NAME, PATH_CLICK_LOG,
           PATH_IMPRESSION_LOG, PATH_SERVER_LOG, DATABASE_CREDENTIALS);
-      CampaignImportHandler importHandler = new CampaignImportHandler(null);
-      boolean importComplete = importHandler.doImport(importConfig);
+      boolean importComplete = controller.imports.doImport(importConfig);
 
-      while (importHandler.isOngoing()) {
-        Thread.sleep(10);
+      while (controller.imports.isOngoing()) {
+        Thread.sleep(100);
       }
-      campaign = importHandler.getImportedCampaign();
+      campaign = controller.imports.getImportedCampaign();
 
       // setup database handler
       if (importComplete) {
-        databaseHandler = new DatabaseHandler(null, DATABASE_CREDENTIALS);
+        databaseHandler = controller.database;
       } else {
         databaseHandler = null;
       }
