3a99cf5df866a1e153d8faf6e365e1a2ba2449ea
David Jones <davidsamueljones@icloud.com>
2018-04-03 01:41:37 +0100

    Put workspace setup in line with requirements for connection setup
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/main/Main.java
+++ src/main/java/group33/seg/main/Main.java
@@ -1,6 +1,8 @@
 package group33.seg.main;
 
+import java.io.FileNotFoundException;
 import group33.seg.controller.DashboardController;
+import group33.seg.controller.database.DatabaseConfig;
 import group33.seg.model.DashboardModel;
 import group33.seg.model.workspace.Workspace;
 import group33.seg.view.DashboardView;
@@ -8,10 +10,18 @@ import group33.seg.view.DashboardView;
 public class Main {
 
   public static void main(String[] args) {
-    
+    // Initialise a test workspace
+    Workspace workspace = new Workspace("Test", "");
+    try {
+      workspace.database = new DatabaseConfig("config.properties");
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+        
     // Create MMC components and link interactions
     DashboardModel model = new DashboardModel();
-    model.setWorkspace(new Workspace("", ""));
+    model.setWorkspace(workspace);
+
     DashboardView view = new DashboardView();
     DashboardController controller = new DashboardController(model, view);
     
