91741d98d3e87fc826d22a96c3eca52744913437
David Jones <davidsamueljones@icloud.com>
2018-04-06 01:07:52 +0100

    Added definition window owned by the view
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/view/DashboardView.java
+++ src/main/java/group33/seg/view/DashboardView.java
@@ -1,16 +1,26 @@
 package group33.seg.view;
 
 import group33.seg.view.structure.DashboardFrame;
+import group33.seg.view.utilities.DefinitionFrame;
 
 public class DashboardView {
   private DashboardFrame dashboard = null;
-
+  private DefinitionFrame definitions = null;
+   
   public DashboardFrame getDashboard() {
     return dashboard;
   }
   
   public void setDashboard(DashboardFrame dashboard) {
     this.dashboard = dashboard;
+  }
+
+  public DefinitionFrame getDefinitions() {
+    return definitions;
+  }
+
+  public void setDefinitions(DefinitionFrame definitions) {
+    this.definitions = definitions;
   }
   
 }
