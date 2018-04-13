087091f9c3c7b8ccb75b1e3f8a7e8ab15c47030e
David Jones <davidsamueljones@icloud.com>
2018-04-13 14:07:56 +0100

    Added empty shell for statistic wizard
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

---
+++ src/main/java/group33/seg/view/controls/StatisticWizardDialog.java
@@ -0,0 +1,21 @@
+package group33.seg.view.controls;
+
+import java.awt.Window;
+import javax.swing.JDialog;
+import group33.seg.controller.DashboardController;
+import group33.seg.model.configs.GraphConfig;
+import group33.seg.model.configs.StatisticConfig;
+
+public class StatisticWizardDialog extends JDialog {
+
+  public StatisticWizardDialog(Window frmCurrent, DashboardController controller,
+      StatisticConfig config) {
+    // TODO Auto-generated constructor stub
+  }
+
+  public StatisticConfig getStatistic() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
