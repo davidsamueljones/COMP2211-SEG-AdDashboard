a4daec84e338ca9526d4ecd3e483b8bebcd53610
David Jones <davidsamueljones@icloud.com>
2018-04-15 02:14:27 +0100

    Added toggle methods for panels
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/view/structure/DashboardFrame.java
+++ src/main/java/group33/seg/view/structure/DashboardFrame.java
@@ -1,6 +1,10 @@
 package group33.seg.view.structure;
 
+import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
@@ -8,18 +12,25 @@ import javax.swing.JSplitPane;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 import group33.seg.controller.DashboardController;
+import group33.seg.view.output.StatisticsView;
 
 public class DashboardFrame extends JFrame {
   private static final long serialVersionUID = 5064629396099335312L;
 
   private DashboardController controller;
+
   private JSplitPane sppMain;
+  private JSplitPane sppView;
+
+  private ControlsPanel pnlControls;
+  private GraphPanel pnlGraph;
+  private StatisticsView pnlStatistics;
 
   /** Create the Dashboard frame. */
   public DashboardFrame(DashboardController controller) {
     // Record the object's controller interface
     this.controller = controller;
-    
+
     this.setTitle("Ad-Dashboard");
     this.setBounds(100, 100, 1280, 720);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,26 +55,68 @@ public class DashboardFrame extends JFrame {
     pnlDashboard.add(sppMain);
 
     // Use LHS as single panel
-    ControlsPanel pnlControls = new ControlsPanel(controller);
+    pnlControls = new ControlsPanel(controller);
     pnlControls.setMinimumSize(pnlControls.getPreferredSize());
     sppMain.setLeftComponent(pnlControls);
-    
+
     // Split RHS into graph and table panels
-    JSplitPane sppView = new JSplitPane();
+    sppView = new JSplitPane();
     sppView.setResizeWeight(1);
-    SwingUtilities.invokeLater(() -> {
-      sppView.setDividerLocation((int) (sppView.getSize().height * 0.75));
-    });
     sppView.setOrientation(JSplitPane.VERTICAL_SPLIT);
-    sppView.setOneTouchExpandable(true);
     sppMain.setRightComponent(sppView);
 
-    GraphPanel pnlGraph = new GraphPanel(controller);
+    pnlGraph = new GraphPanel(controller);
     sppView.setLeftComponent(pnlGraph);
-    
-    WorkspaceStatisticsPanel pnlStatistics = new WorkspaceStatisticsPanel(controller);
+
+    pnlStatistics = new StatisticsView(controller);
     sppView.setRightComponent(pnlStatistics);
 
+    EventQueue.invokeLater(() -> {
+      sppView.setDividerLocation(getDefaultRHSSplit());
+    });
+    
+  }
+  
+  /**
+   * @param show Whether the controls should be shown.
+   */
+  public void showControls(boolean show) {
+    boolean wasShown = pnlControls.isVisible();
+    pnlControls.setVisible(show);
+    if (!show || (!wasShown && show)) {
+      sppMain.setDividerLocation(-1);
+    }
+  }
+
+  /**
+   * @param show Whether the graph view should be shown.
+   */
+  public void showGraph(boolean show) {
+    boolean wasShown = pnlGraph.isVisible();
+    pnlGraph.setVisible(show);
+    if (!show || (!wasShown && show)) {
+      sppView.setDividerLocation(getDefaultRHSSplit());
+      sppMain.setDividerLocation(-1);
+    }
+  }
+
+  /**
+   * @param show Whether the statistics view should be shown.
+   */
+  public void showStatistics(boolean show) {
+    boolean wasShown = pnlStatistics.isVisible();
+    pnlStatistics.setVisible(show);
+    if (!show || (!wasShown && show)) {
+      sppView.setDividerLocation(getDefaultRHSSplit());
+      sppMain.setDividerLocation(-1);
+    }
+  }
+  
+  /**
+   * @return Default split between graph and statistic views
+   */
+  private int getDefaultRHSSplit() {
+    return (int) (sppView.getSize().height * 0.75);
   }
   
 }
