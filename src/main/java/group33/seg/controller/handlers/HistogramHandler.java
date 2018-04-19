6410130ae21dc73df10f586091b215d4813c2af8
David Jones <davidsamueljones@icloud.com>
2018-04-19 23:58:16 +0100

    Added template for histogram handling
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

---
+++ src/main/java/group33/seg/controller/handlers/HistogramHandler.java
@@ -0,0 +1,25 @@
+package group33.seg.controller.handlers;
+
+import group33.seg.model.configs.HistogramConfig;
+
+public class HistogramHandler implements GraphHandlerInterface<HistogramConfig> {
+
+  @Override
+  public void reloadGraph() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void displayGraph(HistogramConfig config) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void clearGraph() {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
