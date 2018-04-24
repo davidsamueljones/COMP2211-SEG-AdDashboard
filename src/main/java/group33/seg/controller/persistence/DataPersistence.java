cfc32b0b5f7de15e2293619b22984174594c7e76
David Jones <davidsamueljones@icloud.com>
2018-04-24 20:55:43 +0100

    Implemented Google's coding standard
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/controller/persistence/DataPersistence.java
+++ src/main/java/group33/seg/controller/persistence/DataPersistence.java
@@ -7,67 +7,67 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
- * Data persistence layer for storage and retrieval of objects in files. Defines default location and file
- * extension of stored objects.
+ * Data persistence layer for storage and retrieval of objects in files. Defines default location
+ * and file extension of stored objects.
  */
 public class DataPersistence {
-	private static final String DIRECTORY = "storage";
-	private static final String FILE_EXTENSION = "adw";
+  private static final String DIRECTORY = "storage";
+  private static final String FILE_EXTENSION = "adw";
 
-	/**
-	 * Do not allow this class to be instantiated.
-	 */
-	public DataPersistence() {}
+  /**
+   * Do not allow this class to be instantiated.
+   */
+  public DataPersistence() {}
 
-	/**
-	 * Store the given object using the file determined from the identifier.
-	 *
-	 * @param identifier Identifier to use for storage
-	 * @param object Object to store
-	 * @throws FileNotFoundException Unable to open file location found using identifier
-	 */
-	public static void store(String identifier, Object object) throws FileNotFoundException {
-		final File file = getFile(identifier);
-		// Make directories if required
-		file.getParentFile().mkdirs();
-		// Create output stream
-		final FileOutputStream fos = new FileOutputStream(file);
-		SerializationUtils.serialize(object, fos);
-		try {
-			fos.close();
-		} catch(IOException e) {
-			// close failed, ignore
-		}
-	}
+  /**
+   * Store the given object using the file determined from the identifier.
+   *
+   * @param identifier Identifier to use for storage
+   * @param object Object to store
+   * @throws FileNotFoundException Unable to open file location found using identifier
+   */
+  public static void store(String identifier, Object object) throws FileNotFoundException {
+    final File file = getFile(identifier);
+    // Make directories if required
+    file.getParentFile().mkdirs();
+    // Create output stream
+    final FileOutputStream fos = new FileOutputStream(file);
+    SerializationUtils.serialize(object, fos);
+    try {
+      fos.close();
+    } catch (IOException e) {
+      // close failed, ignore
+    }
+  }
 
-	/**
-	 * Retrieve an object from the persistence layer with the given identifier.
-	 *
-	 * @param identifier Identifier to try and find stored object for
-	 * @return Object loaded from file
-	 * @throws FileNotFoundException Unable to open file location found using identifier
-	 */
-	public static Object get(String identifier) throws FileNotFoundException {
-		// Open file input stream using identifiers respective file
-		final FileInputStream fis = new FileInputStream(getFile(identifier));
-		// Get object from file input stream
-		final Object object = SerializationUtils.deserialize(fis);
-		try {
-			fis.close();
-		} catch(IOException e) {
-			// close failed, ignore
-		}
-		return object;
-	}
+  /**
+   * Retrieve an object from the persistence layer with the given identifier.
+   *
+   * @param identifier Identifier to try and find stored object for
+   * @return Object loaded from file
+   * @throws FileNotFoundException Unable to open file location found using identifier
+   */
+  public static Object get(String identifier) throws FileNotFoundException {
+    // Open file input stream using identifiers respective file
+    final FileInputStream fis = new FileInputStream(getFile(identifier));
+    // Get object from file input stream
+    final Object object = SerializationUtils.deserialize(fis);
+    try {
+      fis.close();
+    } catch (IOException e) {
+      // close failed, ignore
+    }
+    return object;
+  }
 
-	/**
-	 * Determine the persistence path for an identifier.
-	 *
-	 * @param identifier Identifier to get path (File) for
-	 * @return File that identifier should represent
-	 */
-	private static File getFile(String identifier) {
-		return new File(String.format("%s/%s.%s", DIRECTORY, identifier, FILE_EXTENSION));
-	}
+  /**
+   * Determine the persistence path for an identifier.
+   *
+   * @param identifier Identifier to get path (File) for
+   * @return File that identifier should represent
+   */
+  private static File getFile(String identifier) {
+    return new File(String.format("%s/%s.%s", DIRECTORY, identifier, FILE_EXTENSION));
+  }
 
 }
--- src/main/java/group33/seg/controller/persistence/SerializationUtils.java
+++ src/main/java/group33/seg/controller/persistence/SerializationUtils.java
@@ -13,86 +13,86 @@ import java.io.OutputStream;
  */
 public class SerializationUtils {
 
-	/**
-	 * Do not allow this class to be instantiated.
-	 */
-	private SerializationUtils() {}
+  /**
+   * Do not allow this class to be instantiated.
+   */
+  private SerializationUtils() {}
 
-	/**
-	 * Serialise and then deserialise an object to create a deep clone. This method is slow but is
-	 * provided as an alternative to external libraries.
-	 *
-	 * @param obj Object to clone
-	 * @return Clone of object
-	 */
-	public static Object deepClone(Object obj) {
-		// Serialize into bytes
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		serialize(obj, baos);
-		// Deserialize bytes into object
-		return deserialize(new ByteArrayInputStream(baos.toByteArray()));
-	}
+  /**
+   * Serialise and then deserialise an object to create a deep clone. This method is slow but is
+   * provided as an alternative to external libraries.
+   *
+   * @param obj Object to clone
+   * @return Clone of object
+   */
+  public static Object deepClone(Object obj) {
+    // Serialize into bytes
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    serialize(obj, baos);
+    // Deserialize bytes into object
+    return deserialize(new ByteArrayInputStream(baos.toByteArray()));
+  }
 
-	/**
-	 * Serialse an object to an output stream.
-	 *
-	 * @param obj Object to serialise
-	 * @param os Output stream to target
-	 */
-	public static void serialize(Object obj, OutputStream os) {
-		// Verify arguments
-		if (os == null) {
-			throw new IllegalArgumentException("The OutputStream cannot be null");
-		}
-		// Handle output
-		ObjectOutputStream oos = null;
-		try {
-			oos = new ObjectOutputStream(os);
-			oos.writeObject(obj);
-		} catch(IOException e) {
-			System.err.println("Serialization failed");
-		} finally {
-			try {
-				// Close the stream
-				if (oos != null) {
-					oos.flush();
-					oos.close();
-				}
-			} catch(IOException ex) {
-				// close failed, ignore
-			}
-		}
-	}
+  /**
+   * Serialse an object to an output stream.
+   *
+   * @param obj Object to serialise
+   * @param os Output stream to target
+   */
+  public static void serialize(Object obj, OutputStream os) {
+    // Verify arguments
+    if (os == null) {
+      throw new IllegalArgumentException("The OutputStream cannot be null");
+    }
+    // Handle output
+    ObjectOutputStream oos = null;
+    try {
+      oos = new ObjectOutputStream(os);
+      oos.writeObject(obj);
+    } catch (IOException e) {
+      System.err.println("Serialization failed");
+    } finally {
+      try {
+        // Close the stream
+        if (oos != null) {
+          oos.flush();
+          oos.close();
+        }
+      } catch (IOException ex) {
+        // close failed, ignore
+      }
+    }
+  }
 
-	/**
-	 * Deserialise an object from an input stream.
-	 *
-	 * @param is Input stream to get object from
-	 * @return Object from input stream, returns null on error
-	 */
-	public static Object deserialize(InputStream is) {
-		// Verify arguments
-		if (is == null) {
-			throw new IllegalArgumentException("The InputStream cannot be null");
-		}
-		// Handle input
-		ObjectInputStream ois = null;
-		try {
-			ois = new ObjectInputStream(is);
-			return ois.readObject();
-		} catch (ClassNotFoundException | IOException e) {
-			System.err.println("Deserialization failed");
-			return null;
-		} finally {
-			try {
-				// Close the stream
-				if (ois != null) {
-					ois.close();
-				}
-			} catch(IOException e) {
-				// close failed, ignore
-			}
-		}
-	}
+  /**
+   * Deserialise an object from an input stream.
+   *
+   * @param is Input stream to get object from
+   * @return Object from input stream, returns null on error
+   */
+  public static Object deserialize(InputStream is) {
+    // Verify arguments
+    if (is == null) {
+      throw new IllegalArgumentException("The InputStream cannot be null");
+    }
+    // Handle input
+    ObjectInputStream ois = null;
+    try {
+      ois = new ObjectInputStream(is);
+      return ois.readObject();
+    } catch (ClassNotFoundException | IOException e) {
+      System.err.println("Deserialization failed");
+      return null;
+    } finally {
+      try {
+        // Close the stream
+        if (ois != null) {
+          ois.close();
+        }
+      } catch (IOException e) {
+        // close failed, ignore
+      }
+    }
+  }
 
 }
