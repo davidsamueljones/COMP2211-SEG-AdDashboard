fcfe84196f293b6c8c5b765ceacec7a373b2d9cc
David Jones <davidsamueljones@icloud.com>
2018-04-24 20:54:28 +0100

    Modified the model to store an instance over just the configuration
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/model/DashboardModel.java
+++ src/main/java/group33/seg/model/DashboardModel.java
@@ -1,17 +1,37 @@
 package group33.seg.model;
 
 import group33.seg.model.configs.WorkspaceConfig;
+import group33.seg.model.configs.WorkspaceInstance;
 
 public class DashboardModel {
 
-  private WorkspaceConfig workspace;
+  /** The workspace instance stored by the model */
+  private WorkspaceInstance workspace;
 
+  /**
+   * @return The model's workspace instance's workspace 
+   */
   public WorkspaceConfig getWorkspace() {
-    return workspace;
+    return (workspace == null ? null : workspace.workspace);
   }
 
+  /**
+   * Set the model's workspace configuration using default fields for identifying the workspace
+   * instance.
+   * 
+   * @param workspace Workspace configuration
+   */
   public void setWorkspace(WorkspaceConfig workspace) {
+    setWorkspace(new WorkspaceInstance(null, null, workspace));
+  }
+
+  /**
+   * Set the model's fully defined workspace instance.
+   * 
+   * @param workspace Workspace instance
+   */
+  public void setWorkspace(WorkspaceInstance workspace) {
     this.workspace = workspace;
   }
- 
+
 }
