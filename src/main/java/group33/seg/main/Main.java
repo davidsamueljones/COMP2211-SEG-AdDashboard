5b8f438117093e979841038a799ab6887742e71b
David Jones <davidsamueljones@icloud.com>
2018-04-25 03:53:02 +0100

    Simplified main as now all start logic handled internally
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/main/Main.java
+++ src/main/java/group33/seg/main/Main.java
@@ -15,32 +15,13 @@ import javax.swing.JOptionPane;
 public class Main {
 
   public static void main(String[] args) {
-    // Initialise a test workspace
-    WorkspaceConfig workspace = new WorkspaceConfig();
-    WorkspaceInstance wsi = new WorkspaceInstance("Test", "", workspace);
-    
     // Create MMC components and link interactions
     DashboardModel model = new DashboardModel();
-    model.setWorkspace(workspace);
     DashboardView view = new DashboardView();
     DashboardController controller = new DashboardController(model, view);
-    // Ensure cross-platform appearance is enabled
-    Accessibility.setAppearance(Accessibility.Appearance.PLATFORM);
-
-    final String configuration = "config.properties";
-    try {
-      workspace.database = new DatabaseConfig(configuration);
-      controller.database.refreshConnections(workspace.database, 10);
-      // Start view's dashboard
-      controller.display.openDashboard();
-    } catch (FileNotFoundException e) {
-      JOptionPane.showMessageDialog(null, "Unable to open database connection configuration file, '" + configuration
-              + "', check that it exists", "Error", JOptionPane.ERROR_MESSAGE);
-    } catch (SQLException e) {
-      JOptionPane.showMessageDialog(null, "Unable to create database connections, check that hostname, "
-          + "port, username and password are all correct in configuration file", "Error", JOptionPane.ERROR_MESSAGE);
-    }
-
+    
+    // Start view's dashboard
+    controller.display.openDashboard();
   }
 
 }
