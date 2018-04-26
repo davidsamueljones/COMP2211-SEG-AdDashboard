020445a7ee35149b433d5e247fe23ba5b9f4324d
Harry Brown <harryjgbrown@gmail.com>
2018-04-26 19:06:20 +0100

    added encrypted object serialization support

--- src/main/java/group33/seg/controller/utilities/SerializationUtils.java
+++ src/main/java/group33/seg/controller/utilities/SerializationUtils.java
@@ -1,12 +1,28 @@
 package group33.seg.controller.utilities;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.Serializable;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SealedObject;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Utility class providing static methods for serialising and deserialising of objects.
@@ -95,4 +111,91 @@ public class SerializationUtils {
     }
   }
 
+  private static Cipher makeCipher(char[] password, int opmode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+    SecretKey key64 = new SecretKeySpec(new String(password).getBytes(), "Blowfish");
+    Cipher cipher = Cipher.getInstance("Blowfish");
+    cipher.init(opmode, key64);
+    return cipher;
+  }
+
+  private static Cipher makeEncryptCipher(char[] password) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+    return makeCipher(password, Cipher.ENCRYPT_MODE);
+  }
+
+  private static Cipher makeDecryptCipher(char[] password) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+    return makeCipher(password, Cipher.DECRYPT_MODE);
+  }
+
+  public static void serializeEncrypted(Serializable obj, OutputStream os, char[] password){
+    serializeEncrypted(obj, os, password, true);
+  }
+
+  public static void serializeEncrypted(Serializable obj, OutputStream os, char[] password, boolean clearPasswordAfter) {
+    // Verify arguments
+    if (os == null) {
+      throw new IllegalArgumentException("The OutputStream cannot be null");
+    }
+    // Handle output
+    ObjectOutputStream oos = null;
+    try {
+      Cipher cipher = makeEncryptCipher(password);
+
+      //clear password
+      if(clearPasswordAfter){
+        for(int i=0; i<password.length; i++){
+          password[i] = '0';
+        }
+      }
+
+      SealedObject sealedObject = new SealedObject(obj, cipher);
+      CipherOutputStream cipherOutputStream = new CipherOutputStream(new BufferedOutputStream(os), cipher);
+
+      oos = new ObjectOutputStream(cipherOutputStream);
+      oos.writeObject(sealedObject);
+    } catch (InvalidKeyException | IllegalBlockSizeException | NoSuchAlgorithmException | NoSuchPaddingException | IOException e) {
+      System.err.println("Serialization failed");
+    } finally {
+      try {
+        // Close the stream
+        if (oos != null) {
+          oos.flush();
+          oos.close();
+        }
+      } catch (IOException ex) {
+        // close failed, ignore
+      }
+    }
+  }
+
+  public static Object deserializeEncrypted(InputStream is, char[] password){
+    return deserializeEncrypted(is, password, true);
+  }
+
+  public static Object deserializeEncrypted(InputStream is, char[] password,  boolean clearPasswordAfter){
+    try {
+      Cipher cipher = makeDecryptCipher(password);
+
+      //clear password
+      if(clearPasswordAfter){
+        for(int i=0; i<password.length; i++){
+          password[i] = '0';
+        }
+      }
+      
+      CipherInputStream cipherInputStream = new CipherInputStream( new BufferedInputStream(is), cipher );
+      ObjectInputStream inputStream = new ObjectInputStream( cipherInputStream );
+      
+      SealedObject sealedObject = (SealedObject) inputStream.readObject();
+      Object obj = sealedObject.getObject( cipher );
+
+      inputStream.close();
+      return obj;
+
+    } catch(NoSuchPaddingException | BadPaddingException |
+            NoSuchAlgorithmException | InvalidKeyException |
+            IOException | ClassNotFoundException | IllegalBlockSizeException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 }
---
+++ src/test/java/SerializationTest.java
@@ -0,0 +1,78 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.Test;
+
+import group33.seg.controller.utilities.SerializationUtils;
+
+public class SerializationTest {
+    @Test
+    public void plaintextSerialisationTest() {
+        String testString = "testString";
+        File testFile = new File("test.file");
+
+        try {
+            //serialise object
+            OutputStream os = new FileOutputStream(testFile);
+            SerializationUtils.serialize(testString, os);
+            os.close();
+
+            //deserialise object
+            InputStream is = new FileInputStream(testFile); 
+            Object readObject = SerializationUtils.deserialize(is);
+            is.close();
+
+            testFile.delete();
+
+            //check if objects are equal
+            if (readObject instanceof String) {
+                String readString = (String) readObject;
+                assertTrue("Read object was not the same as the written object", readString.equals(testString));
+            } else {
+                fail("Read object was not the correct type");
+            }
+        } catch (IOException e) {
+            testFile.delete();
+            fail(e.getMessage());
+        }        
+    }
+
+    @Test
+    public void encryptedSerialisationTest() {
+        String testString = "testString";
+        File testFile = new File("test.file");
+
+        char[] examplePassword = "example".toCharArray();
+        try {
+            //serialise object
+            OutputStream os = new FileOutputStream(testFile);
+            SerializationUtils.serializeEncrypted(testString, os, examplePassword, false);
+            os.close();
+
+            //deserialise object
+            InputStream is = new FileInputStream(testFile); 
+            Object readObject = SerializationUtils.deserializeEncrypted(is, examplePassword);
+            is.close();
+
+            testFile.delete();
+
+            //check if objects are equal
+            if (readObject instanceof String) {
+                String readString = (String) readObject;
+                assertTrue("Read object was not the same as the written object", readString.equals(testString));
+            } else {
+                fail("Read object was not the correct type");
+            }
+        } catch (IOException e) {
+            testFile.delete();
+            fail(e.getMessage());
+        }        
+    }
+}
