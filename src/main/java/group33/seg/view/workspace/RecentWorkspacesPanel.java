a65738a3ca67e96f6f081bc12be44b5fab39544d
David Jones <davidsamueljones@icloud.com>
2018-04-26 01:34:12 +0100

    Enabled loading of a recent workspace
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/main/java/group33/seg/view/workspace/RecentWorkspacesPanel.java
+++ src/main/java/group33/seg/view/workspace/RecentWorkspacesPanel.java
@@ -2,21 +2,41 @@ package group33.seg.view.workspace;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 import javax.swing.border.BevelBorder;
+import group33.seg.controller.DashboardController;
+import group33.seg.controller.utilities.ErrorBuilder;
 import group33.seg.model.configs.WorkspaceConfig;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import java.awt.Insets;
+import java.awt.Window;
+import java.util.List;
 import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 
 public class RecentWorkspacesPanel extends JPanel {
   private static final long serialVersionUID = -8972763800588468558L;
 
-  public RecentWorkspacesPanel() {
+  private DashboardController controller;
+
+  private DefaultListModel<String> model_lstWorkspaces;
+  private JList<String> lstWorkspaces;
+
+  /**
+   * Create the panel.
+   *
+   * @param controller Controller for this view object
+   */
+  public RecentWorkspacesPanel(DashboardController controller) {
+    this.controller = controller;
     initGUI();
+    refreshRecent();
   }
 
   private void initGUI() {
@@ -27,7 +47,7 @@ public class RecentWorkspacesPanel extends JPanel {
     gridBagLayout.rowWeights = new double[] {0.0, 1.0, 0.0};
     setLayout(gridBagLayout);
     setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-    
+
     String strHelp = "Select a recently loaded workspace to load:";
     JLabel lblHelp = new JLabel(String.format("<html>%s</html>", strHelp));
     GridBagConstraints gbc_lblHelp = new GridBagConstraints();
@@ -37,7 +57,8 @@ public class RecentWorkspacesPanel extends JPanel {
     gbc_lblHelp.gridy = 0;
     add(lblHelp, gbc_lblHelp);
 
-    JList<WorkspaceConfig> lstWorkspaces = new JList<WorkspaceConfig>();
+    model_lstWorkspaces = new DefaultListModel<>();
+    lstWorkspaces = new JList<String>(model_lstWorkspaces);
     JScrollPane scrWorkspaces = new JScrollPane(lstWorkspaces);
     scrWorkspaces.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
     GridBagConstraints gbc_lstWorkspaces = new GridBagConstraints();
@@ -48,11 +69,44 @@ public class RecentWorkspacesPanel extends JPanel {
     add(scrWorkspaces, gbc_lstWorkspaces);
 
     JButton btnOpen = new JButton("Open");
+    btnOpen.setEnabled(false);
     GridBagConstraints gbc_btnOpen = new GridBagConstraints();
     gbc_btnOpen.anchor = GridBagConstraints.EAST;
     gbc_btnOpen.gridx = 0;
     gbc_btnOpen.gridy = 2;
     add(btnOpen, gbc_btnOpen);
+
+    btnOpen.addActionListener(e -> loadWorkspace());
+    
+    lstWorkspaces.addListSelectionListener(e -> {
+      btnOpen.setEnabled(lstWorkspaces.getSelectedValue() != null);
+    });
+  }
+
+  /**
+   * Refresh the recently displayed workspaces.
+   */
+  public void refreshRecent() {
+    List<String> recents = controller.settings.getRecentWorkspaces();
+    for (String recent : recents) {
+      model_lstWorkspaces.addElement(recent);
+    }
+  }
+
+  /**
+   * Load the currently selected recent workspace.
+   */
+  private void loadWorkspace() {
+    String workspace = lstWorkspaces.getSelectedValue();
+    ErrorBuilder eb = controller.workspace.loadWorkspace(workspace);
+    if (eb.isError()) {
+      JOptionPane.showMessageDialog(null, eb.listComments("Open Workspace"), "Open Workspace",
+          JOptionPane.ERROR_MESSAGE);
+    } else {
+      Window frmCurrent = SwingUtilities.getWindowAncestor(RecentWorkspacesPanel.this);
+      frmCurrent.setVisible(false);
+      frmCurrent.dispose();
+    }
   }
 
 }
